2a91d69164df330119e57ee848034e983a119678
Björn Stierand <bjoern-github@innovention.de>
2025-01-26 14:21:35 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,22 @@
 function Autoampel_Rot_nach_Gruen () {
+    Autoampel_schaltet = 1
     Fussgaenger_Gruen_nach_Rot()
     basic.pause(1000)
     basic.setLedColors(0xff0000, 0xffff00, 0x000000)
     basic.pause(1000)
     basic.setLedColors(0x000000, 0x000000, 0x00ff00)
     Autoampel = 1
+    basic.pause(2000)
+    Autoampel_schaltet = 0
 }
 function Autoampel_Gruen_nach_Rot () {
+    Autoampel_schaltet = 1
     basic.pause(1000)
     basic.setLedColors(0x000000, 0xffff00, 0x000000)
     basic.pause(1000)
     basic.setLedColors(0xff0000, 0x000000, 0x000000)
     Autoampel = 0
+    Autoampel_schaltet = 0
 }
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (Autoampel == 0) {
@@ -20,7 +25,9 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (Fussgaengerampel == 0) {
-        Fussgaenger_Rot_nach_Gruen()
+        if (Autoampel_schaltet == 0) {
+            Fussgaenger_Rot_nach_Gruen()
+        }
     }
 })
 function Fussgaenger_Gruen_nach_Rot () {
@@ -38,9 +45,11 @@ function Fussgaenger_Rot_nach_Gruen () {
     pins.digitalWritePin(DigitalPin.P2, 0)
     Fussgaengerampel = 1
 }
+let Autoampel_schaltet = 0
 let Autoampel = 0
 let Fussgaengerampel = 0
 basic.setLedColors(0xff0000, 0x000000, 0x000000)
 pins.digitalWritePin(DigitalPin.P1, 1)
 Fussgaengerampel = 1
 Autoampel = 0
+Autoampel_schaltet = 0
